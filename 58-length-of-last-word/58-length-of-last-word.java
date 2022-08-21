ad7c6524300e0ac4dd4bf0ce67b4220b4921b83b
jaibatra011 <56126747+jaibatra011@users.noreply.github.com>
2022-08-21 20:01:14 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (52.77%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -1,6 +1,23 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr = s.split(" ");
-        return arr[arr.length-1].length();
+        int ans=0,i=s.length()-1;
+        if(s.charAt(i)==' '){
+            while(i>=0 && s.charAt(i)==' '){
+                i--;
+            }
+            while(i>=0 && s.charAt(i)!=' '){
+                i--;
+                ans++;
+            }
+        }
+        else{
+            while(i>=0 && s.charAt(i)!=' '){
+                i--;
+                ans++;
+            }
+        }
+        return ans;
+        //String[] arr = s.split(" ");
+        //return arr[arr.length-1].length();
     }
 }
