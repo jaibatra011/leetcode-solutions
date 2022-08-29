a373030ddf45e86024bcc826605e3484a90a5fdb
jaibatra011 <56126747+jaibatra011@users.noreply.github.com>
2022-08-29 18:43:13 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (6.68%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int pos=nums1.length-1; //position to put new element
+        // going backwards so that we dont have to shift next elements forward by 1 place
+        int i=m-1,j=n-1;
+        for(;i>=0 && j>=0;){
+            if(nums1[i]>nums2[j]){
+                nums1[pos--]=nums1[i--];
+            }
+            else
+                nums1[pos--]=nums2[j--];
+        }
+        for(;j>=0 && pos>=0;){
+            nums1[pos--]=nums2[j--];
+        }
+        for(;i>=0 && pos>=0;){
+            nums1[pos--]=nums1[i--];
+        }
+    }
+}
