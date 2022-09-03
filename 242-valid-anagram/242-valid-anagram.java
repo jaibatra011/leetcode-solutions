f7b9f1372e697ae3cc25fe041d803a2dbb51e891
jaibatra011 <56126747+jaibatra011@users.noreply.github.com>
2022-09-03 23:57:56 +0530

    Time: 3 ms (95.11%), Space: 41.9 MB (99.89%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -3,11 +3,11 @@ class Solution {
         if(s.length()!=t.length())
             return false;
         int[] count=new int[26];
-        for(Character c:s.toCharArray()){
-            count[c-'a']++;
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
         }
-        for(Character c:t.toCharArray()){
-            count[c-'a']--;
+        for(int i=0;i<t.length();i++){
+            count[t.charAt(i)-'a']--;
         }
         for(int i=0;i<26;i++){
             if(count[i]!=0)
