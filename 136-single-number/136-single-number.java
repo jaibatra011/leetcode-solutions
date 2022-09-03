76b306ea6400b021365cc072a34dde589430b205
jaibatra011 <56126747+jaibatra011@users.noreply.github.com>
2022-09-04 04:04:25 +0530

    Time: 1 ms (100.00%), Space: 50.5 MB (59.45%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int ans=0;
         for(int i:nums) //XOR of all numbers will return single number
-            ans^=i;
+            ans^=i; //double numbers will cancel out each other, p^p=0
         return ans;
     }
 }
