7d5391e4d2aacf886750108da96226667617dfb4
jaibatra011 <56126747+jaibatra011@users.noreply.github.com>
2022-09-07 23:56:05 +0530

    Time: 39 ms (20.49%), Space: 54.3 MB (51.90%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
@@ -19,10 +19,16 @@ class Solution {
             return "";
         String left=tree2str(root.left);
         String right=tree2str(root.right);
-        if(right!="")
+        
+        //if right is not empty string, we don't care what left has, we need to add () to left
+        if(right!="") 
             return root.val+"("+left+")"+"("+right+")";
-        if(left!="")
+        
+        //if it comes till here, means right is empty string, we don't need to add () to right
+        if(left!="") 
             return root.val+"("+left+")";
+        
+        //last case, both left and right is returned empty string, no () needed
         return Integer.toString(root.val);
     }
 }
