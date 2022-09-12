736a4c332749c514dc3b39662db4d20ed9bce4f3
jaibatra011 <56126747+jaibatra011@users.noreply.github.com>
2022-09-12 15:57:39 +0530

    Time: 2 ms (100.00%), Space: 42.2 MB (81.93%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.java
+++ 948-bag-of-tokens/948-bag-of-tokens.java
@@ -1,25 +1,24 @@
 class Solution {
     public int bagOfTokensScore(int[] tokens, int power) {
-        int ans=0;
-        int i=0,j=tokens.length-1;
+        int ans=0, i=0,j=tokens.length-1;
         Arrays.sort(tokens);
         while(i<=j){
-            if(power<tokens[i]){
-                if(ans==0)
+            if(power<tokens[i]){ //face down
+                if(ans==0) // cant face down if ans=0, hence break
                     break;
-                else if(i!=j){
+                else if(i!=j){ 
+                    //no need to face down if i=j, there is no scope of increasing ans further
                     power+=tokens[j--];
                     ans--;
                 }
-                else{
+                else{ // hence if i=j, we just need to get out of loop
                     j--;
                 }
             }
-            else{
+            else{ // face up
                 power-=tokens[i++];
                 ans++;
             }
-            //System.out.println(power);
         }
         return ans;
     }
