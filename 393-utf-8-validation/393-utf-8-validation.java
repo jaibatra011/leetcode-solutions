6d0dad1edf7229c54ddc5d8f77b58daf0c17f9cf
jaibatra011 <56126747+jaibatra011@users.noreply.github.com>
2022-09-13 14:36:14 +0530

    Time: 28 ms (13.47%), Space: 51.4 MB (19.95%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.java
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -1,29 +1,38 @@
 class Solution {
     public boolean validUtf8(int[] data) {
-        int byteCount=0;
+        int byteCount=0; //no of ones in the beginning of each num
         for(int i:data){
-            String s=Integer.toBinaryString(i);
+            String s=Integer.toBinaryString(i); //convert int to binary String
+            
+            //make this string of 8 bits
             s=s.length()>=8?s.substring(s.length()-8):("00000000"+s).substring(s.length());
-            System.out.println(s);
-            if(byteCount==0){
-                for(int j=0;j<8;j++){
+            
+            if(byteCount==0){ // beginning of an n-byte character
+                
+                for(int j=0;j<8;j++){ //calculate no of ones in the beginning
                     if(s.charAt(j)=='1')
                         byteCount++;
                     else
                         break;
                 }
-                if(byteCount!=0 && (byteCount<2 || byteCount>4))
+                
+                //as valid value of byte count is 0,2,3 and 4 so we return false otherwise
+                if(byteCount!=0 && (byteCount<2 || byteCount>4)) 
                     return false;
+                
+                //as we have to check for presense of "10xxxxxx" n-1 times
                 if(byteCount!=0)
                     byteCount--;
             }
-            else{
+            else{//means it is not starting point of n-byte character, we check for "10xxxxxx"
                 if(s.charAt(0)=='1' && s.charAt(1)=='0')
                     byteCount--;
                 else
                     return false;
             }
         }
+        
+        //if byteCount is not 0, means it is expecting some more numbers, hence false, otherwise true
         return byteCount==0;
     }
 }
