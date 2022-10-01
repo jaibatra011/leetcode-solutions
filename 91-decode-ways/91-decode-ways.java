7ca99c4044818fc4553036441b98d1eb36827d09
jaibatra011 <56126747+jaibatra011@users.noreply.github.com>
2022-10-01 20:58:18 +0530

    Time: 3 ms (45.45%), Space: 42.5 MB (45.50%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,26 +1,35 @@
 class Solution {
     public int numDecodings(String s) {
+        // DYNAMIC PROGRAMMING, getting TLE using recursion
         int[] ans=new int[s.length()];
         for(int i=s.length()-1;i>=0;i--){
-            if(s.charAt(i)=='0')
+            //Base case
+            if(s.charAt(i)=='0') //if ith place has zero, no combination possible
                 ans[i]=0;
-            else if(i==s.length()-1)
+            
+            else if(i==s.length()-1) // if string length==1
                 ans[i]=1;
-            else if(i==s.length()-2 && s.charAt(i)!='0'){
-                if(Integer.parseInt(s.substring(i))>0 && Integer.parseInt(s.substring(i))<27 && s.charAt(i+1)!='0')
+            
+            else if(i==s.length()-2){ //if string length==2
+                if(Integer.parseInt(s.substring(i))>0 && Integer.parseInt(s.substring(i))<27 && s.charAt(i+1)!='0') // no zeroes and in range, therefore 2
                     ans[i]=2;
-                else if(Integer.parseInt(s.substring(i))>0 && Integer.parseInt(s.substring(i))<27)
+                else if(Integer.parseInt(s.substring(i))>0 && Integer.parseInt(s.substring(i))<27) // with zeroes but in range, therefore 1, like 10 and 20
                     ans[i]=1;
-                else if(s.charAt(i+1)=='0')
+                else if(s.charAt(i+1)=='0') // not in range with zeroes, no possible ans
                     ans[i]=0;
-                else
+                else // not in range without zeroes, therefore 1 ans possible
                     ans[i]=1;
             }
+            
+            //Calculating using future values
+            // if in range, we can consider single digit and both together
             else if(Integer.parseInt(s.substring(i,i+2))<27)
                 ans[i]=ans[i+1]+ans[i+2];
-            else
+            else // if not in range, we can consider single digit only
                 ans[i]=ans[i+1];
         }
+        
+        //Returning first value
         return ans[0];
     }
 }
