a674f907336f3a5157ebb25b67ea9176a2215726
jaibatra011 <56126747+jaibatra011@users.noreply.github.com>
2022-10-13 13:39:28 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (21.72%) - LeetHub

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -0,0 +1,14 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val=node.next.val; // take next node's value in curr node
+        node.next=node.next.next; // take next node's next in curr node
+    }
+}
